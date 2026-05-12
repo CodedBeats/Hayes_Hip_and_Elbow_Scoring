d2da834d79ec4ad6b823439a1c35bc0dd39235ca
CodedBeats <luca.haar@icloud.com>
2026-05-12 17:43:23 +1000

    feat: success and failed transaction pages (success not fully implemented as it needs to create the firestore doc)

---
+++ app/success/page.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { useEffect } from "react";
+import { useSearchParams } from "next/navigation";
+
+export default function SuccessPage() {
+    const params = useSearchParams();
+
+    useEffect(() => {
+        const finalise = async () => {
+            const sessionId = params.get("session_id");
+
+            if (!sessionId) return;
+
+            const verifyRes = await fetch(
+                `/api/verify-payment?session_id=${sessionId}`,
+            );
+
+            const verifyData = await verifyRes.json();
+
+            if (!verifyData.paid) {
+                alert("Payment not verified");
+                return;
+            }
+
+            const formData = JSON.parse(
+                localStorage.getItem("caseFormData") || "{}",
+            );
+
+            const uploadRefs = JSON.parse(
+                localStorage.getItem("uploadRefs") || "{}",
+            );
+
+            // FINALIZE CASE HERE
+            console.log(formData);
+            console.log(uploadRefs);
+
+            // create firestore doc here
+        };
+
+        finalise();
+    }, [params]);
+
+    return <div>Payment successful</div>;
+}
